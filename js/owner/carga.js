e74da7c0373c42553c56909325f3e9c3c561c524
Juan Palma <juan.palma@me.com>
2022-02-08 11:46:15 -0600

    Redes sociales

---
+++ js/owner/carga.js
@@ -0,0 +1,13 @@
+let cargaAll = 0;
+/*
+requirejs.config({
+	baseUrl: 'js/owner',
+	paths: {
+		a: '../animaciones',
+		l: '../librerias',
+		lottie:'https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.8.1/lottie.min'
+	}
+});
+
+requirejs ([ 'main' ]) ;
+*/
